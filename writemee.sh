755d0cd79c65b408804e89ede0fb3d0df6cfcb49
Uma BasaveswaraRao Kurra <kurraumabasava@gmail.com>
2024-10-21 16:13:19 +0530

    helloscript

---
+++ writemee.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ echo " please enter your username"
+
+ read -s USERNAME
+
+ echo " please enter your password"
+
+ read -s PASSWORD
+
+ echo "username is: $USERNAME, password is: $PASSWORD
+
