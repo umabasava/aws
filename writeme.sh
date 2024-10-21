3ce57425817d451676aa11f7a849dcee5870799d
Uma BasaveswaraRao Kurra <kurraumabasava@gmail.com>
2024-10-21 15:43:17 +0530

    hello

--- writeme.sh
+++ writeme.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-echo "ramesh:: hi suresh how are you?"
-echo "suresh:: hi ramesh iam fine what about you?"
-echo "ramesh:: iam also fine suresh
+echo "suresh:: hi bro"
+echo "ramesh:: hello man"
