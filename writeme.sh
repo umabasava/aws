515758822438f0b9bbfd69eaaf750cbfe5180a1b
Uma BasaveswaraRao Kurra <kurraumabasava@gmail.com>
2024-10-21 16:00:05 +0530

    person

--- writeme.sh
+++ writeme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PERSON1=RAMESH
-PERSON2=SURESH
+PERSON1=$1
+PERSON2=$2
 echo "$PERSON1:: hi man whats up"
 echo "$PERSON2:: hello $person1 iam fine"
 echo "$PERSON1:: iam also fine $PERSON2"
