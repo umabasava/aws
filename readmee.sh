0f15579d201e19e0e9b5dac727fc906386c7498c
Uma BasaveswaraRao Kurra <kurraumabasava@gmail.com>
2024-10-25 17:17:59 +0530

    learns

--- readmee.sh
+++ readmee.sh
@@ -4,4 +4,4 @@ echo "Please enter the username:: "
 read USERNAME
 echo "enter the password::"
 read PASSWORD
-echo "username is :USERNAME, password is :: PASSWORD"
+echo "username is :$USERNAME, password is :$PASSWORD"
