7f546722675f701bade3606732d8669cf5f2eb69
Uma BasaveswaraRao Kurra <kurraumabasava@gmail.com>
2024-10-25 15:55:49 +0530

    persons

--- readmee.sh
+++ readmee.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- echo  "ramesh:: hii suresh"
- echo  "suresh:: hii ramesh"
- echo  "ramesh:: how are you suresh"
- echo  "suresh:: iam fine ramesh" 
+ echo "ramesh:: hii suresh"
+ echo "suresh:: hii ramesh"
+ echo "ramesh: how are you suresh"
+ echo "suresh:: iam fine ramesh" 
