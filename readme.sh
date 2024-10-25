22ad95fc9a09357c1a74fa65d5b2561d0fc095c8
Uma BasaveswaraRao Kurra <kurraumabasava@gmail.com>
2024-10-25 17:39:18 +0530

    learns

--- readme.sh
+++ readme.sh
@@ -3,4 +3,4 @@ MOVIES=("KING" "QUEEN" "KINGS")
 echo "First movie is: ${MOVIES[0]} "
 echo "Second movie is: ${MOVIES[1]} "
 echo "Third movie is: ${MOVIES[2]} "
-echo "all movies are: ${MOVIES[3]}"
+echo "all movies are: ${MOVIES[@]}"
