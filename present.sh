a1f4192ac5f3364fc1c22f719a8d3599e9ceca0a
Uma BasaveswaraRao Kurra <kurraumabasava@gmail.com>
2024-11-05 15:06:46 +0530

    hello

---
+++ present.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+COURSE="Devops from current script"
+echo "Before calling other script, course: $COURSE"
+echo "process ID of PRESENT SCRIPT: $$"
+#./other.sh 
+echo "After calling the other script, course: $COURSE"
